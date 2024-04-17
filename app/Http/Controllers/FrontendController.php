903701c07ee7b449170a32e928761e9e6385753a
shajar123 <129936713+shajar123@users.noreply.github.com>
2024-04-17 20:40:11 +0500

    frontend pages added

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -9,4 +9,28 @@ class FrontendController extends Controller
     public function dashboard(){
         return view('frontend.includes.dashboard');
     }
+    public function blogs(){
+        return view('frontend.pages.blog');
+    }
+    public function contact_us(){
+        return view('frontend.pages.contact-us');
+    }
+    public function category(){
+        return view('frontend.pages.category');
+    }
+    public function products(){
+        return view('frontend.pages.products');
+    }
+    public function wishlist(){
+        return view('frontend.elements.wishlist');
+    }
+    public function shopping_cart(){
+        return view('frontend.elements.shopping-cart');
+    }
+    public function about_us(){
+        return view('frontend.elements.about-us');
+    }
+    public function checkout(){
+        return view('frontend.elements.checkout');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,22 @@ use \App\Http\Controllers\FrontendController;
 
 
 Route::get('/admin/dashboard',[DashboardController::class,'index']);
-Route::get('/', [FrontendController::class, 'dashboard']);
+Route::get('/', [FrontendController::class, 'dashboard'])->name('dashboard');
+Route::get('/blogs', [FrontendController::class, 'blogs'])->name('blogs');
+Route::get('/contact-us', [FrontendController::class, 'contact_us'])->name('contact.us');
+Route::get('/Category', [FrontendController::class, 'category'])->name('category');
+Route::get('/products', [FrontendController::class, 'products'])->name('products');
+Route::get('/wishlist', [FrontendController::class, 'wishlist'])->name('wishlist');
+Route::get('/shopping-cart', [FrontendController::class, 'shopping_cart'])->name('shopping.cart');
+Route::get('/about-us', [FrontendController::class, 'about_us'])->name('about.us');
+Route::get('/checkout', [FrontendController::class, 'checkout'])->name('checkout');
+
+
+
+
+
+
+
+
+
 
